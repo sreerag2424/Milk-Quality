ae005d94603fc76b86939ca27cf2f2d8b3595498
Sreerag biju <sreeragbiju2004@gmail.com>
2025-01-28 12:48:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ ta = st.number_input("Insert Taste of Milk (0/1)")
 om = st.number_input("Insert Odor of Milk (0/1)")
 fm = st.number_input("Insert Fat of Milk (0/1)")
 tu = st.number_input("Insert Turbidity of Milk (0/1)")
-cm = st.number_input("Insert Colour of Milk (240-255))")
+cm = st.number_input("Insert Colour of Milk (240-255)")
 if st.button("Predict"):
     pred = lr_modle.predict(np.array([[ph,tm,ta,om,fm,tu,cm]]))
     st.write("Quality of Milk is :",pred[0])
